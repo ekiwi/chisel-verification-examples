bae78a5546224d21443308a3a38f6bb48342abf2
Kevin Läufer <laeufer@cs.berkeley.edu>
2022-02-17 17:08:23 -0800

    bmc and induction example

--- test/SMTSolverExample.scala
+++ test/SMTSolverExample.scala
@@ -98,12 +98,75 @@ class SMTSolverExample extends AnyFreeSpec {
 
     assert(isBad(s0))
   }
+
+  // this uses an example from the zipcpu slides:
+  // https://zipcpu.com/tutorial/class-verilog.pdf (slide 99)
+  "demonstrate bounded model checking with counter example" in {
+    val (bv0, bv1, bv5, bv22, bv500) = (BVLiteral(0, 16), BVLiteral(1, 16), BVLiteral(5, 16), BVLiteral(22, 16), BVLiteral(500, 16))
+    case class State(counter: BVSymbol, enable: BVSymbol)
+    def makeState(num: Int): State = State(BVSymbol(s"counter_$num", 16), BVSymbol(s"enable_$num", 1))
+    def init(s: State): BVExpr = BVEqual(s.counter, bv0)
+    def next(s0: State, s1: State): BVExpr = {
+      val nextCounter = BVIte(s0.enable,
+        BVIte(BVEqual(s0.counter, bv22), bv0, BVOp(Op.Add, s0.counter, bv1)),
+        s0.counter
+      )
+      // enable is an input and thus left unconstrained
+      BVEqual(s1.counter, nextCounter)
+    }
+    def bad(s: State): BVExpr = {
+      val assert0 = BVNot(BVEqual(s.counter, bv500))
+      BVNot(assert0)
+    }
+
+    // manual bmc
+    val (s0, s1) = (makeState(0), makeState(1))
+
+    // check if the initial state violates the assertion
+    assert(solve(Seq(init(s0), bad(s0))).isEmpty)
+    // check if state 1 can violate the assertion
+    assert(solve(Seq(init(s0), next(s0, s1), bad(s1))).isEmpty)
+
+    // implement a bmc routine
+    def bmc(k: Int): Unit = {
+      var prev = makeState(0)
+      var cond = List(init(prev))
+      (0 to k).foreach { j =>
+        val r = solve(cond :+ bad(prev))
+        if(r.nonEmpty) {
+          println(s"found bug after $j steps")
+          println(r.toSeq.sorted.mkString("\n"))
+          return
+        }
+        // add another state
+        val state = makeState(j + 1)
+        cond = cond :+ next(prev, state)
+        prev = state
+      }
+    }
+
+    bmc(10)
+
+    // induction step w/o strengthening
+    val ind0 = solve(Seq(BVNot(bad(s0)), next(s0, s1), bad(s1)))
+    assert(ind0.nonEmpty, "without a strengthening invariant it should not pass")
+    println(ind0)
+
+    // induction step w/ strengthening
+    def inv(s: State): BVExpr = BVComparison(Compare.GreaterEqual, bv22, s.counter, false)
+    val check = solve(Seq(inv(s0), next(s0, s1), BVNot(inv(s1))))
+    assert(check.isEmpty)
+    val ind1 = solve(Seq(BVNot(bad(s0)), inv(s0), next(s0, s1), bad(s1)))
+    assert(ind1.isEmpty)
+  }
 }
 
 
 /** helper function to easily check SMT formulas and return all assignments */
 object solve {
   private val verbose: Boolean = false
+
+  def apply(expr: Seq[BVExpr]): Map[String, BigInt] = apply(BVAnd(expr.toList))
 
   def apply(expr: BVExpr): Map[String, BigInt] = {
     require(expr.width == 1, s"Expression needs to be boolean, not ${expr.width}-bits wide.")
